22b7fd023b307341b3fd05a8453e15411cf1969c
amir karimi <takfashomal@gmail.com>
2023-06-25 16:40:41 +0330

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
+    css: [
     {src:'~/assets/css/style.css'},
-  ],
-  runtimeConfig: {
-    SecretToken: '',
-    AppMode: 'debug',
-    public: {
-      apiBase: 'https://api.arta20.top/api/',
-      baseUrl: 'https://api.arta20.top',
-    }
-  },
+    ],
+
 })
